5bd08c32fe0f17f84b9f2cc1e5d0150811f381ee
nima64 <kennethe407@gmail.com>
2024-04-27 18:54:39 -0700

    made homepage :

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import AddIcon from '@mui/icons-material/Add';
+
+function OpenCamera() {
+  return <Button variant="contained">Open Camera</Button>;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+        />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous"
+        />
       </header>
+      <div>
+        <div style={{backgroundColor: "#b8e9b8"}} className="row">
+          <div  className="mx-auto text-center col-md-8">
+            <div className="flex">
+
+            </div>
+            <img style={{width:"10rem"}}  src="/recyc.png" className="App-logo" alt="logo" />
+            <h1 style={{fontWeight:"800"}} >WE RECYCLE</h1>
+          </div>
+
+        </div>
+        <div className="row">
+          <div className="p-5 mx-auto text-center col-md-8">
+            <h1 className="fw-bolder">Welcome</h1>
+          </div>
+        </div>
+        <div className="row">
+
+          <div className="mx-auto text-center col">
+            <CameraAltIcon style={{fontSize:"4rem", fill:"green"}} />
+          </div>
+          <div className="col">
+            <h3>Scan your item</h3>
+          </div>
+        </div>
+        <div className="row m-5">
+          <div className="mx-auto text-center col">
+          <AddIcon style={{fill:"green", fontSize:"6rem"}} />
+          </div>
+          <div className="col">
+            <h3>We can tell if the item can be recycled</h3>
+          </div>
+        </div>
+        <div className="row m-5">
+          <div className="mx-auto text-center ">
+            <Button variant="contained">Open Camera</Button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="text-center ">
+            <Button variant="contained">Sign in </Button>
+          </div>
+        </div>
+      </div>
+      <script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"
+      ></script>
     </div>
   );
 }
---
+++ src/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Homepage</h1>
+            <p>This is the homepage of our site.</p>
+        </div>
+    );
+}
+
+export default HomePage;
